6a17021f0c73105ab4ad99683143cf3784636a65
Amanda Araskog <73289643+araskog@users.noreply.github.com>
2020-11-11 09:36:50 +0100

    Add files via upload

---
+++ src/cardList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './card';
+
+const CardList = ({ cats }) => {
+	return  (
+		<div>
+		{ 
+			cats.map((user, i) => {
+				return (
+				<Card 
+				key={cats[i].id} 
+				id={cats[i].id} 
+				name={cats[i].name} 
+				email={cats[i].email} 
+				/>
+				);
+			})
+		}	
+		</div>
+	);
+}
+
+export default CardList;
